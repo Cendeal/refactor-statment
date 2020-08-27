815b48c26cad2efd2bce4d47a25faf51fccfbea5
Chence5 <chence5@oocl.com>
2020-08-27 15:23:26 +0800

    [refactor][cendeal]refactor: add class Performance and class statement replace string concat

--- src/statement.js
+++ src/statement.js
@@ -1,12 +1,43 @@
+const format = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+}).format;
+
+class Performance{
+  constructor(name,total,seats) {
+    this.name=name
+    this.total = total
+    this.seats = seats
+  }
+  toString(){
+    return ` ${this.name}: ${format(this.total)} (${this.seats} seats)`
+  }
+}
+
+class Statement{
+  constructor(owner,credits,amount,performances) {
+    this.owner = owner
+    this.credits = credits
+    this.amount = amount
+    this.performances = performances
+  }
+  printText(){
+    return `Statement for ${this.owner}\n` +
+        this.performances.map(performance=>performance.toString()+'\n').join('')+
+        `Amount owed is ${format(this.amount)}\n` +
+        `You earned ${this.credits} credits \n`
+  }
+  printHtml(){
+
+  }
+}
+
 function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
+  const performances = []
+
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
@@ -31,13 +62,11 @@ function statement (invoice, plays) {
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    //print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
+    const performance = new Performance(play.name,thisAmount/100,perf.audience)
+    performances.push(performance)
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits \n`;
-  return result;
+  return new Statement(invoice.customer,volumeCredits,totalAmount/100,performances).printText()
 }
 
 module.exports = {
